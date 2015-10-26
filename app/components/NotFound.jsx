dd3f526b9abbc2d48a40cebdf269474dc5edf9b0
jcheunga <jasoncheunga@gmail.com>
2015-10-26 19:48:45 +1100

    completednotfound

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default class NotFound extends React.Component {
 	render() {
 		return (
-			<div className="sep-top-lg sep-bottom-lg"><h1>404 Not Found</h1></div>			
+			<div className="sep-top-lg sep-bottom-lg text-center">
+				<h4>404 Not Found</h4>
+			</div>			
 		);
 	}
 }
--- app/routes.jsx
+++ app/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Route from 'react-router';
+import { Route } from 'react-router';
 
 import App from 'components/App';
 import About from 'components/About';
@@ -9,12 +9,12 @@ import Collection from 'components/change/Collection';
 import SingleCollection from 'components/change/SingleCollection';
 import NotFound from 'components/NotFound';
 
-export default (
-  <Route component={App}>
-  	<NotFoundRoute component={NotFound}/>
+export default (	
+  <Route component={App}>  	
     <Route path="/" component={Change} />    
     <Route path="about" component={About} />
     <Route path="collections" component={Collection} />
     <Route path="collection/:collectionId" component={SingleCollection} />
+  	<Route path="*" component={NotFound}/>
   </Route>
 );
