22c3e4d29921186a29ff98242596b311c447de96
jcheunga <jasoncheunga@gmail.com>
2015-10-29 01:14:48 +1100

    currently

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,13 +9,13 @@ export default class Footer extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/collections">Collections</Link>
+              <small><Link to="/">Home</Link></small>
+              <small><Link to="/about">About</Link></small>
+              <small><Link to="/collections">Collections</Link></small>        
               </div>
             </div>
             <div className="row">
-              <div className="col-md-12 text-center sep-top-xs"><small>&copy; Copyright Preview 2015.</small></div>
+              <div className="col-md-12 text-center sep-top-xs">&copy; Copyright Preview 2015.</div>
             </div>
           </div>
         </div>
--- app/components/components/change/HomeInitial.jsx
+++ app/components/components/change/HomeInitial.jsx
@@ -9,6 +9,7 @@ export default class HomeInitial extends React.Component {
               <h5>Step 1</h5>
               <i className="fa fa-eye homeicon"></i>
               <p>Copy the URL of a product and input the CATEGORY and URL above.</p>
+              <p>Currently works with Amazon and Backcountry</p>
           </div>
           <div className="col-md-4 text-center sep-top-md sep-bottom-md">
               <h5>Step 2</h5>
